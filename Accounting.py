c8ffcda77e4368a2c7339a17dc3dbda2ba0577ec
jay yeh <leo700322@gmail.com>
2022-08-11 01:47:45 +0800

    refactor

--- Accounting.py
+++ Accounting.py
@@ -2,39 +2,53 @@
 
 import os # operating system
 
-products = []
-
 # 讀取檔案
-if os.path.isfile('products.csv'): # 檢查檔案在不在
-    print('Yeah! Found it!')
-    with open('products.csv', 'r', encoding='utf-8') as f:
+def read_file(filename):
+    products = []
+    with open(filename, 'r', encoding='utf-8') as f:
         for line in f:
             if '商品,價格' in line:
                 continue # 這次迴圈下面的程式碼跳過，從上面再重新執行一次。
             name, price = line.strip().split(',')
             products.append([name, price])
-    print(products)
-
-else:
-    print('Can not found it!')
+    return products
 
 # 讓使用者輸入
-while True:
-    name = input('Please input the product name: ')
-    if name == 'q':
-        break
-    price = input('Please input the price:')
-    price = int(price)
-    products.append([name, price])
-print(products)
+def user_input(products):
+    while True:
+        name = input('Please input the product name: ')
+        if name == 'q':
+            break
+        price = input('Please input the price: ')
+        price = int(price)
+        products.append([name, price])
+    print(products)
+    return products
 
 # 印出所有購買紀錄
-for p in products:
-    print(p[0], '的價格是', p[1])
+def print_products(products):
+    for p in products:
+        print(p[0], '的價格是', p[1])
 
 # 寫入檔案
-with open('products.csv', 'w', encoding='utf-8') as f:
-    f.write('商品, 價格\n')
-    for p in products:
-        f.write(p[0] + ',' + str(p[1]) + '\n')
+def write_file(filename, products):
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write('商品, 價格\n')
+        for p in products:
+            f.write(p[0] + ',' + str(p[1]) + '\n')
 
+
+
+def main():
+    filename = 'products.csv'
+    if os.path.isfile(filename):
+        print('Yeah! Found it!')
+        products = read_file(filename)
+    else:
+        print('Can not found file.')
+
+    products = user_input(products)
+    print_products(products)
+    write_file(filename, products)
+
+main()
